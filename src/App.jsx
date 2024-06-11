5ba4fbd74b688a294963be27a14044c5a496c0e8
fariasagustin3 <fariasagustin3@gmail.com>
2024-06-11 11:50:55 -0300

    index page created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { Link } from "react-router-dom"
+
 function App() {
   return (
-    <div>
-      <h1 className='font-bold underline'>Hello World</h1>
+    <div className="w-screen h-screen" style={{ backgroundImage: "url('/assets/image-background.png')", backgroundSize: "cover" }}>
+      <div className="w-screen h-screen bg-black opacity-80 flex flex-col items-center justify-center gap-3 px-5">
+        <h1 className="text-[var(--white-color)] text-center">¡Bienvenido!</h1>
+        <p className="text-[var(--white-color)] text-center">Bienvenido al menu del restaurante. Presiona el botón debajo para continuar</p>
+        <Link to="/menu">
+          <button className="bg-[var(--white-color)] px-10 py-2 text-[var(--black-color)] rounded-full text-sm font-medium">
+            ¡Vamos!
+          </button>
+        </Link>
+      </div>
     </div>
   )
 }
